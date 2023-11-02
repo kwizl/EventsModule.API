0bbf130087754e5249b59c876f16896bdce9df15
martinn <martinn@innova.co.ke>
2023-11-02 19:06:11 +0300

    Build OpenIdConnect Interactive Client for MVC Application

--- src/EventsModule.Client/Program.cs
+++ src/EventsModule.Client/Program.cs
@@ -1,12 +1,32 @@
 using EventsModule.Client.ApiServices;
-using EventsModule.Data.Context;
-using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddScoped<IEventApiService, EventApiService>();
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
+})
+.AddCookie(CookieAuthenticationDefaults.AuthenticationScheme)
+.AddOpenIdConnect(OpenIdConnectDefaults.AuthenticationScheme, options =>
+{
+    options.Authority = "https://localhost:7165";
+
+    options.ClientId = "eventsModule_mvc_client";
+    options.ClientSecret = "secret";
+    options.ResponseType = "code";
+
+    options.Scope.Add("openid");
+    options.Scope.Add("profile");
+
+    options.SaveTokens = true;
+    options.GetClaimsFromUserInfoEndpoint = true;
+});
 
 var app = builder.Build();
 
@@ -23,6 +43,7 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
